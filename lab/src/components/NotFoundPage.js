487c1eed1a2a679d9839fda11c9cfcd8a5e6be8e
William Le Ferrand <william.le-ferrand@polytechnique.edu>
2017-03-22 20:56:34 -0700

    smaller file, better not found page

--- lab/src/components/NotFoundPage.js
+++ lab/src/components/NotFoundPage.js
@@ -9,7 +9,7 @@ export default class NotFoundPage extends React.Component {
       <div className="not-found container-fluid">
         <div className="row justify-content-center">
           <div className="col-4">
-            <img src="/img/robot2.jpg" className="img-fluid img-thumbnail" alt="404" />
+            <img src="/img/robot2.JPG" className="img-fluid img-thumbnail" alt="404" />
         </div>
         </div>
         <div className="row justify-content-center">
