5938a4fdcf8c3f9f251b01ae99c973f6fd5c5cba
William Le Ferrand <william.le-ferrand@polytechnique.edu>
2017-03-22 17:41:22 -0700

    adding redirect

--- lab/src/app-client.js
+++ lab/src/app-client.js
@@ -4,9 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './components/AppRoutes';
 
-// set up stripe
-//var stripe = Stripe('pk_test_KNYK2I1UsxoIXX0jDiG46mmj');
-
 window.onload = () => {
-    ReactDOM.render(<AppRoutes />, document.getElementById('main'));
+    if (location.protocol != 'https:')
+    {
+        location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    } else {
+        ReactDOM.render(<AppRoutes />, document.getElementById('main'));
+    }
 };
