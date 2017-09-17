99758098a9912246a0bae986f474f07234b26a10
wleferrand <william.le-ferrand@polytechnique.edu>
2017-09-17 23:57:28 +0200

    adding king components

---
+++ lab/src/catalog/King.js
@@ -0,0 +1,23 @@
+import Instrument from './../models/Instrument.js';
+import {fabric} from 'fabric';
+
+function factory(name, width, height) {
+    return(new Instrument(name, function() {
+	return (new fabric.Rect({
+	    top : 0,
+	    left : 0,
+	    width : width,
+	    height : height,
+	    hasControls : false,
+	    fill : 'blue'
+	}))
+    }));
+}
+
+
+var KX170 = factory("KX 170/175", 6.25, 2.5)
+var KN64 = factory("KN 64", 6.3, 1.3)
+
+module.exports = {
+    KX170, KN64
+}
