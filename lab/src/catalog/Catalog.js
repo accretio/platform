1915d9df4f21bf81d32d3ea625f7f59cf97569b3
wleferrand <william.le-ferrand@polytechnique.edu>
2017-09-17 16:58:09 +0200

    adding new files

--- lab/src/catalog/Catalog.js
+++ lab/src/catalog/Catalog.js
@@ -1,11 +1,13 @@
 'use strict';
 
-import { GTX345 } from './GTX345.js';
-import { GNS430 } from './GNS430.js';
+import { GTX345, GTX335, GNS430, GNS530, GTN650, GTN750, G500 } from './Garmin.js';
+import { JPI790 } from './JPI.js'
 import { ROUND3125 } from './ROUND3125.js';
 
 const catalog =
-    [ GTX345, GNS430, ROUND3125 ]
+      [ GTX345, GTX335, GNS430, GNS530, GTN650, GTN750, G500,
+	JPI790,
+	ROUND3125 ]
 
 function findInstrument(name) {
     console.log("looking for instrument " + name)
