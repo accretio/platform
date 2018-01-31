b2d51ad454d736f07929b41d4b4a022cd0610dfb
wleferrand <william.le-ferrand@polytechnique.edu>
2018-01-31 15:22:31 +0100

    updating the index page

--- lab/src/routes.js
+++ lab/src/routes.js
@@ -5,9 +5,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import Layout from './components/Layout';
 import LandingPage from './components/LandingPage';
-import PanelEditor from './components/PanelEditor';
 import NotFoundPage from './components/NotFoundPage';
-import SelectLayout from './components/SelectLayout';
 import SuggestDestination from './components/SuggestDestination';
 import ReviewDestinations from './components/ReviewDestinations';
 
@@ -18,10 +16,7 @@ const routes = (
         <IndexRoute component={LandingPage}/>
 	<Route path="/suggestDestination" component={SuggestDestination} />
 	<Route path="/reviewDestinations" component={ReviewDestinations} />
-
-	<Route path="/layout" component={SelectLayout} />
-	<Route path="/panel/:id" component={PanelEditor} />
-        <Route path="*" component={NotFoundPage}/>
+	<Route path="*" component={NotFoundPage}/>
 	
         </Route>
 );
